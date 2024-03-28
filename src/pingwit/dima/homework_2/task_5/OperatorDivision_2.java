ac68f9f4e382dda235e0a20bd5c0ace396fe4159
dmitrijpapu <andreevich8437357@gmail.com>
2024-03-28 19:49:45 +0100

    New project

--- src/pingwit/dima/homework_2/task_5/OperatorDivision_2.java
+++ src/pingwit/dima/homework_2/task_5/OperatorDivision_2.java
@@ -4,7 +4,7 @@ public class OperatorDivision_2 { // убери из названия класс
     public static void main(String[] args) {
         int num_5 = 8;
         int num_6 = 3;
-        int result_1 = num_6 % num_5; // _1 в названии переменной не нужно и старайся не использовать таких названий переменных
-        System.out.println(result_1);
+        int result = num_6 % num_5; // _1 в названии переменной не нужно и старайся не использовать таких названий переменных
+        System.out.println(result);
     }
 }
--- src/pingwit/dima/homework_3/task_1/CurrencyExchange.java
+++ src/pingwit/dima/homework_3/task_1/CurrencyExchange.java
@@ -2,13 +2,13 @@ package pingwit.dima.homework_3.task_1;
 
 public class CurrencyExchange {
     public static void main(String[] args) {
-        int $ = 1345; // технически название переменной верное, но не называй так -> usd
+        int USD = 1345; // технически название переменной верное, но не называй так -> usd
         int currencyId = 5;
         switch (currencyId) {
-            case 1 -> System.out.println(($ * 39) + "₴");
-            case 2 -> System.out.println(($ * 151) + "¥");
-            case 3 -> System.out.println(($ * 7) + "¥");
-            case 4 -> System.out.println(($ / 2) + "€");
+            case 1 -> System.out.println((USD * 39) + "₴");
+            case 2 -> System.out.println((USD * 151) + "¥");
+            case 3 -> System.out.println((USD * 7) + "¥");
+            case 4 -> System.out.println((USD / 2) + "€");
             default -> System.out.println("Ваш банк не может обменять данную валюту");
 // лишняя строка, удали
         }
